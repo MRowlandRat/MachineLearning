bccbdd6331dc4fe04e094448d73cf00b73174d80
AGersonNEU <98633255+AGersonNEU@users.noreply.github.com>
2023-03-06 17:30:37 -0700

    Pushing my whole project, everyhting works but no css

---
+++ Models/SpaceMissionModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MachineLearning.Models
+{
+    public class SpaceMissionModel
+    {
+        //Company_Name = @"CASC",
+        //        Location = @"Site 9401 (SLS-2), Jiuquan Satellite Launch Center, China",
+        //        Datum = @"Thu Aug 06, 2020 04:01 UTC",
+        //        Detail = @"Long March 2D | Gaofen-9 04 & Q-SAT",
+        //        Status_Rocket = @"StatusActive",
+        //        Rocket = @"29.75",
+
+       
+        public string? companyName { get; set; }
+
+        public string? location { get; set; }
+
+        public string? statusRocket { get; set; }
+
+        public string? rocketCost { get; set; }
+
+        public SpaceMissionModel(string companyName, string location, string statusRocket, string rocketCost)
+        {
+            this.companyName = companyName;
+            this.location = location;
+            this.statusRocket = statusRocket;
+            this.rocketCost = rocketCost;
+        }
+
+        public SpaceMissionModel()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"Inputs:\n      Company Name: {companyName} \n      Location: {location} \n     Rocket Status: {statusRocket} \n    Rocket Cost: {rocketCost}";
+        }
+
+    }
+}
