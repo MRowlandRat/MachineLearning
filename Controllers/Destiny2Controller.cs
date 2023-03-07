711c21434d0dd7fac207f251ae5aaab971827825
soliddrogue <sbhattarai@student.neumont.edu>
2023-03-07 16:55:41 -0700

    completed

---
+++ Controllers/Destiny2Controller.cs
@@ -0,0 +1,31 @@
+﻿
+using MachineLearning.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+
+namespace MachineLearning.Controllers
+{
+    public class Destiny2Controller : Controller
+    {
+        Destiny2 c = new Destiny2();
+
+        [HttpGet]
+        public IActionResult Destiny2()
+        {
+            return View();
+        }
+
+        
+        public IActionResult Destiny2_Prediction(D2Guns gun)
+        {
+
+
+            string prediction = c.destiny2Predictions(gun.Gun_name, gun.Gun_archtype, gun.Source, gun.Element, gun.Gun_Rof, gun.Weapon_type);
+            ViewBag.prediction = prediction;
+
+            return View();
+        }
+
+    }
+}
---
+++ Models/Destiny2.cs
@@ -0,0 +1,34 @@
+﻿namespace MachineLearning.Models
+{
+
+
+    public class Destiny2
+    {
+        public Destiny2() { }
+
+        public string destiny2Predictions(String Gun_Name, String Gun_Archtype, String Source, String Element, float RateofFire, String Weapon_Type)
+        {
+            var sampleData = new MLMSav.ModelInput()
+            {
+
+
+                Gun_name = Gun_Name,
+                Gun_archetype = Gun_Archtype,
+                Source = Source,
+                Element = Element,
+                Gun_RoF = RateofFire,
+                Weapon_type = Weapon_Type
+
+            };
+
+            //Load model and predict output
+            var result = MLMSav.Predict(sampleData);
+
+            return result.PredictedLabel.ToString();
+
+        }
+
+        
+
+    }
+}
